e0ebece5704d8f2804ac2e4181e1c3bfea125b1a
Gabriel Zapodeanu <gzapodea@cisco.com>
2019-01-26 17:27:18 -0500

    update for CL Barcelona

---
+++ config.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+
+# developed by Gabi Zapodeanu, TSA, GPO, Cisco Systems
+
+# This file contains the Spark Auth, ServiceNow Dev account info, router info
+
+WEBEX_TEAMS_URL = 'https://api.ciscospark.com/v1'
+WEBEX_TEAMS_AUTH = 'Bearer ' + 'Paste your token here'
+WEBEX_TEAMS_ROOM = 'Config Change Alert'
+WEBEX_TEAMS_MEMBER = 'your email'
+
+PDX_RN = '10.93.130.42'
+
+SW_HOST = 'L3 SW IP Address'
+RO_HOST = 'DC Router IP Address'
+PORT = 830
+USER ='username'
+PASS ='password'
+
+SNOW_URL = 'https://dev12345.service-now.com/api/now'
+SNOW_ADMIN = 'username'
+SNOW_DEV = 'ERNA'
+SNOW_PASS = 'password'
+SNOW_INSTANCE = 'dev12345'
+
+
+GOOGLE_API_KEY = 'Google API KEY'
+
+DNAC_URL = 'https://1.2.3.4'
+DNAC_USER = 'admin'
+DNAC_PASS = 'password'
+
+ASAv_URL = 'https://10.93.130.40'
+ASAv_USER = 'admin'
+ASAv_PASSW = 'password'
+
+UCSD_URL = 'https://ucsd ip address'
+UCSD_USER = 'user'
+UCSD_PASSW = 'password'
+UCSD_KEY = 'key'
+
+APPROVER_EMAIL = 'your email address'
